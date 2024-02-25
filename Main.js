b8d30803e213d5f88e4e4c6d3f19652eb62339f4
unknown <artur.gevorgyan.04@list.ru>
2024-02-25 14:54:38 +0400

    Calculator

--- Main.js
+++ Main.js
@@ -1,39 +1,64 @@
 let numbers = document.querySelectorAll(".btn");
 let screen = document.getElementById('b');
-let screen1 = document.getElementById('b');
 let uses = document.querySelectorAll(".bt");
+let hakadarc = document.getElementById("pm")
+let del = document.getElementById("AC");
+let point = document.getElementById("point")
+let x = 0;
+let y = 0;
+let btn;
 
 numbers.forEach(number =>{
     number.addEventListener("click",()=>{
         screen.value +=  number.textContent})
 })    
 
-
-// if(screen.value === ""){
-//     numbers.forEach(number1 =>{
-//         number1.addEventListener("click",()=>{
-//             screen1.value +=  number1.textContent})
-// })  
-// }
-
-
 uses.forEach(use =>{
     use.addEventListener("click",()=>{
-        if (use.textContent === "+") {
-            screen.value = "";
+        if (use.textContent === "=") {
+          y = +screen.value;
+          calculateResult();
+        }else{
+            btn = use.textContent;
+            x = +screen.value;
+            screen.value = '';
         }
-        else if(use.textContent === "%"){
-            screen.value=screen.value / 100;
+        if(use.textContent === "%"){
+            screen.value = x / 100;
         }
     })
 })
 
 
+hakadarc.addEventListener("click", ()=>{
+    screen.value = -1 * x;
+})
+point.addEventListener("click", ()=>{
+    screen.value = x + '.';
+})
 
+function calculateResult() {
+    let result;
+    switch (btn) {
+        case "+":
+            result = x + y;
+            break;
+        case "-":
+            result = x - y;
+            break;
+        case "*":
+            result = x * y;
+            break;
+        case "/":
+            result = x / y;
+            break;
+        default:
+            result = "Error";
+    }
+    screen.value = result;
+}
 
-let del = document.getElementById("AC");
 del.addEventListener("click",()=>{
     screen.value = null;
 })
-
 
